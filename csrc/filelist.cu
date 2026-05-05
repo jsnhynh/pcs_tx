a875565e297ce8fb279370f978d42b63f12f3f0d
Jason Huynh2 <019175806@coe-ee-cad2.sjsuad.sjsu.edu>
2026-05-05 13:31:53 -0700

    res in pass/fail imp

--- csrc/filelist.cu
+++ csrc/filelist.cu
@@ -4,16 +4,16 @@ ifeq ($(LDVERSION), 0)
 endif
 
 ARCHIVE_OBJS=
-ARCHIVE_OBJS += _3966649_archive_1.so
-_3966649_archive_1.so : archive.2/_3966649_archive_1.a
+ARCHIVE_OBJS += _3969504_archive_1.so
+_3969504_archive_1.so : archive.3/_3969504_archive_1.a
 	@$(AR) -s $<
-	@$(PIC_LD) -shared  -z notext -m elf_i386  -Bsymbolic $(LD_NORELAX_FLAG)  -o .//../simv.daidir//_3966649_archive_1.so --whole-archive $< --no-whole-archive
+	@$(PIC_LD) -shared  -z notext -m elf_i386  -Bsymbolic $(LD_NORELAX_FLAG)  -o .//../simv.daidir//_3969504_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
-	@ln -sf .//../simv.daidir//_3966649_archive_1.so $@
+	@ln -sf .//../simv.daidir//_3969504_archive_1.so $@
 
 
 ARCHIVE_OBJS += _prev_archive_1.so
-_prev_archive_1.so : archive.2/_prev_archive_1.a
+_prev_archive_1.so : archive.3/_prev_archive_1.a
 	@$(AR) -s $<
 	@$(PIC_LD) -shared  -z notext -m elf_i386  -Bsymbolic $(LD_NORELAX_FLAG)  -o .//../simv.daidir//_prev_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
